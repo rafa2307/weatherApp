0421d1aa65c25c1faed04a931b1fbdf21fccf99c
Rafael Loredo <rafaelloredo41@yahoo.com>
2025-02-01 17:41:58 -0600

    Improved error handling

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function getWeather() {
     const city = document.getElementById('city').value;
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.innerText = "";
+
+    if(city === ""){
+        errorMessage.innerText = "Please enter a city name.";
+        return;
+    }
     const apiKey = API_KEY;
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok){
+                throw new Error("City not found.");
+            }
+            return response.json();
+        })
         .then(data => {
+            //console.log("API Response:" , data)
+            if(!data.main || !data.weather || data.weather.length === 0){
+                throw new Error("Weather data unavailable.");
+            }
+
             const location = data.name;
             const temp = data.main.temp;
             const description = data.weather[0].description;
@@ -15,6 +32,6 @@ function getWeather() {
             document.getElementById('description').innerText = `Description: ${description}`;
         })
         .catch(error => {
-            alert('City not found. Please try again.');
+            errorMessage.innerText = error.message;
         });
 }
