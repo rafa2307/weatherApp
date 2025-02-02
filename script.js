c31cbee05f0ee65778d510c6475f3fcf54b30770
Rafael Loredo <rafaelloredo41@yahoo.com>
2025-02-01 18:45:33 -0600

    Added geolocation

--- script.js
+++ script.js
@@ -47,3 +47,61 @@ function getWeather() {
             loadingMessage.style.display = "none";
         });
 }
+
+function getWeatherByLocation() {
+    const errorMessage = document.getElementById('error-message');
+    const loadingMessage = document.getElementById('loading-message');
+
+    errorMessage.innerText = "";
+    loadingMessage.style.display = "none";
+
+    if(!navigator.geolocation){
+        errorMessage.innerText = "Geolocation is not supported by your browser."
+        return;
+    }
+
+    loadingMessage.style.display = "block";
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            fetchWeatherByCoord(lat, lon);
+        }, 
+        (error) => {
+            errorMessage.innerText = "Unable to retrieve your location.";
+            loadingMessage.style.display = "none";
+        }
+    );
+}
+
+function fetchWeatherByCoord(lat, lon){
+    const apiKey = API_KEY;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+    fetch(apiUrl)
+        .then(response => {
+            // Checking if the user entered a fake city
+            if (!response.ok){
+                throw new Error("Weather data unavailable.");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const location = data.name;
+            const temp = data.main.temp;
+            const description = data.weather[0].description;
+
+            document.getElementById('location').innerText = `Location: ${location}`;
+            document.getElementById('temp').innerText = `Temperature: ${temp}`;
+            document.getElementById('description').innerText = `Description: ${description}`;
+        })
+        .catch(error => {
+            errorMessage.innerText = error.message;
+        })
+        .finally(() => {
+            loadingMessage.style.display = "none";
+        });
+}
+
+document.getElementById('location-btn').addEventListener('click', getWeatherByLocation);
